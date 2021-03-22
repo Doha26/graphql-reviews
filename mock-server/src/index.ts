1612bbdba9dcbc04a67fc01cfa1539d870df6d1d
Pavel <foujeupavel@gmail.com>
2021-03-23 00:53:46 +0100

    mock-server

--- mock-server/src/index.ts
+++ mock-server/src/index.ts
@@ -3,7 +3,9 @@ import cors from 'cors';
 import * as reviews from './db/reviews.json'
 import * as items from './db/items.json'
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT !== undefined ? parseInt(process.env.PORT) : 3000 || 3000;
+const host = '0.0.0.0'
+
 const app = express();
 app.use(cors())
 
@@ -20,6 +22,10 @@ app.get('/items', (req: express.Request, res: express.Response) => {
     return res.status(200).json(items)
 })
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+app.listen(port, host, 1, () => {
+    console.log(`
+    Server started on: 
+    Port:${port}
+    Host: ${host} 
+    `);
 })
